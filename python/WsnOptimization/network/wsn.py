70d7c2e703319b1124a89deafab2dbb69db7db17
Edielson P. Frigieri <edielsonpf@gmail.com>
2017-01-30 20:44:36 -0200

    fixing python bug related to float rounding

--- python/WsnOptimization/network/wsn.py
+++ python/WsnOptimization/network/wsn.py
@@ -87,21 +87,21 @@ class WSN(object):
         # generated a graph with connection between nodes initially unconnected
         self.__graph = [[self.__unconnected for i in range(self.__numNodes)] for j in range(self.__numNodes)]
         
-        ValidConnection = False
-        
         for node_a in self.__nodes:
+            ValidConnection = False
             for node_b in self.__nodes:
                 if node_a != node_b:
                     dist = self.__calcEuclidianDist(node_a,node_b)
-                    if dist < self.__caldDistance(self.__power[self.__nodes.index(node_a)]):
+                    if round(dist,2) <= round(self.__caldDistance(self.__power[self.__nodes.index(node_a)]),2):
                         ValidConnection = True
                         self.__graph[self.__nodes.index(node_a)][self.__nodes.index(node_b)] = dist
                         self.__nxGraph.add_edge(self.__nodes.index(node_a), self.__nodes.index(node_b)) 
         
-        if ValidConnection == False:
-            logging.warning('The graph is not connected!')
-            self.__graph = []
-            self.__nxGraph=[]
+            if ValidConnection == False:
+                logging.warning('The graph is not connected!\r\n')
+                self.__graph = []
+                self.__nxGraph=[]
+                break
             
     def getScenario(self):
         return self.__nodes
@@ -110,26 +110,33 @@ class WSN(object):
         return self.__graph
     
     def optimizePower(self):
-        
-        #optimize each row, finding the farthest node
-        for i in range(len(self.__graph)):
-            highDistance = 0
-            for j in range(len(self.__graph)):
-                if (i!=j) & (self.__graph[i][j] < self.__unconnected) & (self.__graph[i][j] > highDistance):
-                    highDistance = self.__graph[i][j]  
-
-            #calculate the power of the transmitter for achieving the minimum power level 
-            self.__power[i] = self.__threshold+self.__calcLoss(highDistance);
-            if self.__power[i] < self.__minTxLevel:
-                self.__power[i] = self.__minTxLevel
-        
+        if(self.__graph):
+            #optimize each row, finding the farthest node
+            for i in range(len(self.__graph)):
+                highDistance = 0
+                for j in range(len(self.__graph)):
+                    if (i!=j) & (self.__graph[i][j] < self.__unconnected) & (self.__graph[i][j] > highDistance):
+                        highDistance = self.__graph[i][j]  
+    
+                #calculate the power of the transmitter for achieving the minimum power level 
+                self.__power[i] = self.__threshold+self.__calcLoss(highDistance);
+                if self.__power[i] < self.__minTxLevel:
+                    self.__power[i] = self.__minTxLevel
+        else:
+            logging.warning('The graph is not valid!\r\n')
+            self.__power = []
+            
         return self.__power
     
     def optimizeTopology(self):
-        self.__graph = self.__mst.prim(self.__graph)
-        self.__mst.printMST()
-        self.optimizePower()
-        self.__generateGraph()
+        if(self.__graph):
+            self.__graph = self.__mst.prim(self.__graph)
+            self.__mst.printMST()
+            self.optimizePower()
+            self.__generateGraph()
+        else:
+            logging.warning('The graph is not valid!\r\n')
+            self.__power = []    
         return self.__power            
                                 
     def getLinks(self):
@@ -153,28 +160,28 @@ class WSN(object):
         -------
         
         """
-        
-        if positions == None:
-            #in this case it is necessary o convert the matrix node to dictionary    
-            pos=0
-            positions={}
-            for node in self.__nodes:
-                positions[pos]=np.array([node[0],node[1]])
-                pos+=1
+        if(self.__graph):
+            if positions == None:
+                #in this case it is necessary o convert the matrix node to dictionary    
+                pos=0
+                positions={}
+                for node in self.__nodes:
+                    positions[pos]=np.array([node[0],node[1]])
+                    pos+=1
+                
+            # nodes
+            nx.draw_networkx_nodes(self.__nxGraph,positions,node_size=100)
+                
+            # edges
+    #         nx.draw_networkx_edges(self.__nxGraph,positions,edgelist=elarge,width=2)
+    #         nx.draw_networkx_edges(self.__nxGraph,positions,edgelist=esmall,width=2,alpha=0.5,edge_color='b',style='dashed')
+            nx.draw_networkx_edges(self.__nxGraph,positions,width=2)
             
-        # nodes
-        nx.draw_networkx_nodes(self.__nxGraph,positions,node_size=100)
+            # labels
+            nx.draw_networkx_labels(self.__nxGraph,positions,font_size=20,font_family='sans-serif')
             
-        # edges
-#         nx.draw_networkx_edges(self.__nxGraph,positions,edgelist=elarge,width=2)
-#         nx.draw_networkx_edges(self.__nxGraph,positions,edgelist=esmall,width=2,alpha=0.5,edge_color='b',style='dashed')
-        nx.draw_networkx_edges(self.__nxGraph,positions,width=2)
-        
-        # labels
-        nx.draw_networkx_labels(self.__nxGraph,positions,font_size=20,font_family='sans-serif')
-        
-        plt.axis('off')
-        #plt.savefig("weighted_graph.png") # save as png
-#         plt.show(block=False)
-        plt.show() # display
+            plt.axis('off')
+            #plt.savefig("weighted_graph.png") # save as png
+    #         plt.show(block=False)
+            plt.show() # display
         
